43efd6a1dd57d010282016f6e8c76ff3c5837b5a
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-02-01 14:26:13 +0100

    first commit

---
+++ src/components/AppCards.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    props: {
+        project: Object,
+    },
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5>{{ project.title }}</h5>
+        </div>
+    </div>
+</template>
+
+<style></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from 'axios';
+import AppCards from './AppCards.vue';
+export default {
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+            projects: []
+
+        }
+    },
+    created() {
+        axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
+            // console.log(resp);
+            this.projects = resp.data.results;
+        })
+    },
+    components : {AppCards}
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <h2>Progetti disponibili:</h2>
+        <div class="row">
+            <div class="col" v-for="project in projects" :key="project.id">
+               <AppCards :project="project"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped ></style>
